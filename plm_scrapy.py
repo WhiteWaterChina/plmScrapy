67f07bb8f7092ccc7ae561f439bb73c2d8ad2d60
yanshuo <yanshuo5091@163.com>
2017-08-11 16:04:50 +0800

    20170811

--- plm_scrapy.py
+++ plm_scrapy.py
@@ -2,7 +2,7 @@
 # -*- coding:cp936 -*-
 import time
 import os
-import codecs
+import xlsxwriter
 import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -10,6 +10,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import tkMessageBox
+import datetime
 
 
 print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
@@ -22,7 +23,6 @@ browser.get(url)
 WebDriverWait(browser, 3600).until(ec.presence_of_all_elements_located((By.XPATH, "//div[@class='x-grid3-cell-inner x-grid3-col-ASSIGNMENT_SUBJECT']")))
 time.sleep(5)
 scrollbar_test = browser.find_element_by_xpath("//div[contains(@id, 'wrap_user-homepage_tab_jcaGen')]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]")
-filename = codecs.open('个人PLM上传文档数量.csv'.decode('gbk'), 'w', 'gbk')
 
 
 def get_data():
@@ -50,11 +50,32 @@ for i in range(0, 100):
     scrollbar_test.send_keys(Keys.PAGE_DOWN)
     time.sleep(5)
 
-for index, item in enumerate(file_doc_list):
-    filename.write(item + "," + file_time_list[index] + os.linesep)
+
+timestamp = time.strftime('%Y%m%d', time.localtime())
+workbook_to_write = xlsxwriter.Workbook("个人PLM上传文档数量-%s.xlsx".decode('gbk') % timestamp)
+formatone = workbook_to_write.add_format()
+formatone.set_border(1)
+sheet_now = workbook_to_write.add_worksheet("个人PLM上传文档数量".decode('gbk'))
+sheet_now.set_column('A:A', 100)
+sheet_now.set_column('B:B', 25)
+formattitle = workbook_to_write.add_format()
+formattitle.set_border(1)
+formattitle.set_align('center')
+formattitle.set_bg_color("yellow")
+formattitle.set_bold(True)
+
+list_title = ["报告标题".decode('gbk'), "报告上传时间".decode('gbk')]
+
+sheet_now.merge_range(0, 0, 0, 2, "个人PLM上传文档数量".decode('gbk'), formattitle)
+
+for index_title, item_title in enumerate(list_title):
+    sheet_now.write(1, index_title, item_title, formatone)
+
+for index_data, item_data in enumerate(file_doc_list):
+    sheet_now.write(index_data + 2, 0, item_data, formatone)
+    sheet_now.write(index_data + 2, 1, datetime.datetime.strptime(file_time_list[index_data], '%Y/%m/%d'), workbook_to_write.add_format({'num_format': 'yyyy-mm-dd', 'border': 1}))
+workbook_to_write.close()
 length_filedoc_list = len(file_doc_list)
-print length_filedoc_list
-filename.close()
 browser.quit()
 print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-tkMessageBox.showinfo('完成啦'.decode('gbk'), '数据抓取完毕，共抓取到%s个结果！保存在《个人PLM上传文档数量.csv》中'.decode('gbk') %length_filedoc_list)
+tkMessageBox.showinfo('完成啦'.decode('gbk'), '数据抓取完毕，共抓取到%s个结果！保存在《个人PLM上传文档数量-%s.xlsx》中'.decode('gbk') % (timestamp, length_filedoc_list))
